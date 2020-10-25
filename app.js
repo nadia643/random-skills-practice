ccd206db3e5e15275aba036d5c203438469c4293
John Doe <nadia643@gmail.com>
2020-10-25 17:36:44 +0000

    ammended initial slide index

--- app.js
+++ app.js
@@ -36,4 +36,22 @@ function changeColour() {
 
 
 // REVIEWS SLIDER
-let reviews = []
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+} 
+
+function showDivs(n) {
+  let i;
+  let x = document.getElementsByClassName("image-slide");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+
